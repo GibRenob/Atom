2b29e74bc5c535a22137a1efee0e2a89365d307f
Christine Miller <christinemiller@Christine-Millers-MacBook-Air.local>
2016-08-04 11:03:07 -0600

    HW

--- Midterm/map.js
+++
@@ -1,9 +0,0 @@
-var map;
-function initMap() {
-  map = new google.maps.Map(document.getElementById('map'), {
-    center: {lat: 40.518, lng: -90.672},
-    zoom: 2,
-      mapTypeId: google.maps.MapTypeId.SATELLITE
-    });
-    map.setTilt(45);
-  }
